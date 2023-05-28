80a92c0e2e18cd3864b7f1b281f20fd02721a470
Domantelio✨ <domantelio@proton.me>
2023-05-28 12:02:50 +0300

    Added byte serialization and deserialization

--- Feedback/Models/Attachment.cs
+++ Feedback/Models/Attachment.cs
@@ -1,9 +1,35 @@
+namespace Feedback.Models;
+
 using System.IO;
 using System.IO.Enumeration;
-namespace Feedback.Models;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text;
 
 public class Attachment : Entity {
     public long FileSize { get; set; }
     public string FileType { get; set; }
     public string FileName { get; set; }
+    [JsonConverter(typeof(Base64EncodedStringConverter))]
+    public byte[] Bytes { get; set; }
+    // public Base64 Bytes { get; set; }
+    // public bytes[] Bytes { get; set; }
+}
+
+public class Base64EncodedStringConverter : JsonConverter<byte []>
+{
+    public override byte[] Read(
+        ref Utf8JsonReader reader, 
+        Type typeToConvert, 
+        JsonSerializerOptions options)
+    {
+        return reader.GetBytesFromBase64();
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer, 
+        byte[] value, 
+        JsonSerializerOptions options
+    ) =>
+        writer.WriteString("bytes", Convert.ToBase64String(value));
 }
--- Feedback/Program.cs
+++ Feedback/Program.cs
@@ -1,10 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-// using Microsoft.Extensions.DependencyInjection.Abstractions;
 
 using Feedback.DB;
 using Feedback.Repositories;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
